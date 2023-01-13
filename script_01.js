aef8f314ae6f581febe833798e6f3318bf184978
FISIDAN <schwark@posteo.de>
2023-01-13 14:32:20 +0100

    bis Zeile 33 untypisierten Sprache

--- script_01.js
+++ script_01.js
@@ -14,10 +14,20 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; //Deklaration bzw Definition der Variablen in Camel-Schreibweise
-firstName = "Daniel"; // Hier wird der Wert Daniel an die Variable firstName übergeben Ziel = Wert
-console.log(firstName); //Ausgabe
+// let firstName; //Deklaration bzw Definition der Variablen in Camel-Schreibweise
+// firstName = "Daniel"; // Hier wird der Wert Daniel an die Variable firstName übergeben Ziel = Wert
+// console.log(firstName); //Ausgabe
 
-let lastName = "Schwark" // DEklaration und WErtzuweisung in einer Zeile
-console.log(lastName);
-console.log(firstName +" "+ lastName); //kombinierte Ausgabe mit Leerzeichen
+// let lastName = "Schwark" // DEklaration und WErtzuweisung in einer Zeile
+// console.log(lastName);
+// console.log(firstName +" "+ lastName); //kombinierte Ausgabe mit Leerzeichen
+
+/* JS ist eine untypisierte Sprache! | untyped */
+
+let test;
+// test = "hallo";
+// test = 2;
+test = true;
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
